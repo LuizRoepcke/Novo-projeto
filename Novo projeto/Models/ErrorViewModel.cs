ef1d173f5f602468420bd28dcdf803b962a35aed
Luiz Felipe Roepcke <Luiz_roepcke@estudante.sc.senai.br>
2023-12-26 19:07:37 -0300

    project created

---
+++ Novo projeto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Novo_projeto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
